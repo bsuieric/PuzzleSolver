cd45f599d32879295cd6050b6870f03033ba95ca
Bogdan Suierica <bogdansuierica@yahoo.com>
2014-12-21 20:13:01 +0100

    +relazione
    relazione boh

--- primaParte/src/puzzle/PuzzleSolver.java
+++ primaParte/src/puzzle/PuzzleSolver.java
@@ -6,7 +6,6 @@ import puzzle.IOWriter;
 
 public class PuzzleSolver {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		String inputPath = args[0];
 	    String outputPath = args[1];
 		Puzzle puzzle=new Puzzle(inputPath);
