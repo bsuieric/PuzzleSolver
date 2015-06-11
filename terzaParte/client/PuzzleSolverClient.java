1dae8e4caa5dbf0842fc6d83f5232aa7266e1a24
Bogdan Suierica <bogdansuierica@yahoo.com>
2015-06-11 14:58:31 +0200

    Modificato il diagramma

--- terzaParte/client/PuzzleSolverClient.java
+++ terzaParte/client/PuzzleSolverClient.java
@@ -4,27 +4,36 @@ import shared.Puzzle;
 import shared.SolverAlgorithm;
 
 public class PuzzleSolverClient {
-	
-		public static void main(String[] args)throws Exception{
+	private static final String HOST = "localhost";	
+	public static void main(String[] args)throws Exception{
 
-			String inputPath = args[0];
-			String outputPath = args[1];
-			String host = args[2];
-			Puzzle puzzle=new Puzzle();
-			Puzzle p=new Puzzle();
-		    IOReader reader=new IOReader();
-		    reader.readContent(inputPath, puzzle);
-		    try{
-		    	SolverAlgorithm ref=(SolverAlgorithm) Naming.lookup("rmi://"+host+"/Hello");
-		    	p=ref.solve(puzzle);
-		    }
-		    catch(ConnectException e){
-		    	System.out.println("problemi di connessionie a server!");
-		    }
-		    catch(Exception exc){
-		    	exc.printStackTrace();
-		    }
-			IOWriter writer=new IOWriter();
-			writer.print(p, outputPath);
+	    String inputPath = null;
+		String outputPath = null;
+		String name = null;
+		try{
+			inputPath = args[0];
+			outputPath = args[1];
+			name = args[2];
+		}catch(ArrayIndexOutOfBoundsException excep){
+			System.err.println("Give input file, output file and server name");
+			System.exit(1);
+		}
+
+		Puzzle puzzle=new Puzzle();
+		Puzzle p=new Puzzle();
+	    IOReader reader=new IOReader();
+	    reader.readContent(inputPath, puzzle);
+	    try{
+	    	SolverAlgorithm ref=(SolverAlgorithm) Naming.lookup("rmi://"+HOST+"/"+ name);
+	    	p=ref.solve(puzzle);
+	    }
+	    catch(ConnectException e){
+	    	System.out.println("problemi di connessionie a server!");
+	    }
+	    catch(Exception exc){
+	    	exc.printStackTrace();
+	    }
+		IOWriter writer=new IOWriter();
+		writer.print(p, outputPath);
 	}
 }
--- terzaParte/server/PuzzleSolverServer.java
+++ terzaParte/server/PuzzleSolverServer.java
@@ -1,14 +1,30 @@
 package server;
 import java.rmi.*;
 import server.PuzzleToSolve;
+import java.net.MalformedURLException;
 
 public class PuzzleSolverServer {
-	
+	private static final String HOST = "localhost";
 	public static void main(String[] args)throws Exception{
-        String host = args[0];
+        String name = null;
+        try{
+        	name = args[0];
+        }catch(ArrayIndexOutOfBoundsException e){
+        	System.err.println("Select a server name!");
+        	System.exit(1);
+        }
+
 		PuzzleToSolve pts=new PuzzleToSolve();
-		String rmiObjName="rmi://"+host+"/Hello";
-		Naming.rebind(rmiObjName, pts);
+		String rmiObjName="rmi://"+HOST+"/" + name;
+		try{
+			Naming.rebind(rmiObjName, pts);
+		}catch(RemoteException exec){
+			System.err.println("Error during the rebind!");
+			System.exit(1);
+		}catch(MalformedURLException exec){
+			System.err.println("Malformed URL!");
+			System.exit(1);
+		}
 		System.out.println("Server Pronto");
 	}
 }
